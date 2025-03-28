70ebb7d4a1f5280f9e283a3f7eb3c54b2506b21b
thaliawoods <thaliadelano@gmail.com>
2025-03-28 17:27:49 +0100

    Image & Text Homepage

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import HomeImage from "@/components/HomeImage";
+import HomeText from "@/components/HomeText";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-grow pt-20 pb-16">
-        <h1>Bienvenue sur JardinSolidaire</h1>
-        <p>Ceci est la page d&apos;accueil de votre site.</p>
+        <HomeImage />
+        <HomeText />
       </main>
       <Footer />
     </div>
