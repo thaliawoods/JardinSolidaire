ccd822a9a3b48a97dbcd4de6dfb5a825b136a1d2
Hanaë <vernonhanae@gmail.com>
2025-05-19 10:22:59 +0200

    ajout backend de jardin

--- backend/server.js
+++ backend/server.js
@@ -33,13 +33,24 @@
 // });
 
 const express = require('express');
+const cors = require('cors');
+const jardinsRoutes = require('./routes/jardins');
+const jardiniersRoutes = require('./routes/jardiniers');
+
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/jardins', jardinsRoutes);
+app.use('/api/jardiniers', jardiniersRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Backend is running!');
+  res.send('Backend avec Prisma est en ligne ✅');
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`✅ Serveur lancé sur le port ${PORT}`);
 });
+
