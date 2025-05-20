f07535d77f112166aa6fc90737f47c417400e28c
Hanaë <vernonhanae@gmail.com>
2025-05-20 15:57:05 +0200

    jardins et jardniers update

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,28 @@
 require('dotenv').config();
+console.log('▶️ DATABASE_URL =', process.env.DATABASE_URL);
+
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
 
-
 const prisma = new PrismaClient();
 
-const cors = require('cors');
+
 const jardinsRoutes = require('./routes/jardins');
 const jardiniersRoutes = require('./routes/jardiniers');
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use('/api/jardins', jardinsRoutes);
-app.use('/api/jardiniers', jardiniersRoutes);
 
 app.use(cors());
 app.use(express.json());
+app.use('/api/jardins', jardinsRoutes);
+app.use('/api/jardiniers', jardiniersRoutes);
+
 
 app.get('/', (req, res) => {
   res.send('Backend avec Prisma est en ligne ✅');
